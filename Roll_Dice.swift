89760d0f13f55e1682cc1c04e48ee517972a0c2f
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-14 22:24:16 -0800

    Update Roll_Dice.swift

--- Roll_Dice.swift
+++ Roll_Dice.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOulet allows me to reference a UI element
+    //When you want to rename a UI element reference, go to refactor and rename. Do not just change. 
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
