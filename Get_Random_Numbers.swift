906a22f295124954b1e3800c0e2179a396801bea
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-17 17:27:15 -0800

    Update Get_Random_Numbers.swift

--- Get_Random_Numbers.swift
+++ Get_Random_Numbers.swift
@@ -1,3 +1,9 @@
 //Print random number within 0 to 100
 
 print(Int.random(in:0...100))
+
+
+//If there is an array of elements:
+
+var myFriends = [Tori, Ann, Jordan, Brad, Mimi, Jesus, Callie, Emma]
+print(myFriends.randomElement())
