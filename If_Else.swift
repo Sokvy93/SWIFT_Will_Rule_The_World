6a873d7e96af687c7fbcbe3220cbd6f1310e2b20
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-25 23:14:51 -0800

    Update If_Else.swift

--- If_Else.swift
+++ If_Else.swift
@@ -5,3 +5,17 @@ if trafficLight == "green" {
 } else { 
     stop() 
 }
+
+----------------------------------------
+
+func loveCalculator() {
+    let loveScore = Int.random(in: 0...100)
+    
+    if loveScore == 100 {
+        print("You love each other like Kanye loves Kanye")
+    } else {
+        print("You'll be forever alone")
+    }
+}
+
+loveCalculator()
