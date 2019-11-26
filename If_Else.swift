c0ff7c05045fdc6048944bc04f5d1c90cbd01c20
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-25 23:21:52 -0800

    Update If_Else.swift

--- If_Else.swift
+++ If_Else.swift
@@ -11,8 +11,10 @@ if trafficLight == "green" {
 func loveCalculator() {
     let loveScore = Int.random(in: 0...100)
     
-    if loveScore == 100 {
+    if loveScore > 80 {
         print("You love each other like Kanye loves Kanye")
+    } else if loveScore > 40 {
+        print("You go together like COke and Mentos")
     } else {
         print("You'll be forever alone")
     }
